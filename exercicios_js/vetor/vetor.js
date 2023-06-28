bc2cac6e8a61f7b8aad6af42ae9f2f51360fa7ab
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2023-06-28 11:47:13 -0300

    Update vetor.js

--- exercicios_js/vetor/vetor.js
+++ exercicios_js/vetor/vetor.js
@@ -7,19 +7,29 @@ frm.addEventListener("submit",(e)=>{
     const frase = frm.texto.value
     const vetor = frase.split(";")
 
-    let palavravetor = []
     let palavra = []
+    let indices = []
     for (let i = 0; i < vetor[0].length; i++) {
         palavra.push(frase[i])
+        if(i>1)
+        indices.push(vetor[i])
     }
     
+    alterarTexto(palavra,vetor[1],vetor)
+    
+})
+
+function alterarTexto(palavra,addTexto,vetor) {
+    let palavravetor = []
+
     for (let i = 0; i < palavra.length; i++) {
         for (let j = 2; j < vetor.length; j++) {
             if (i == Number(vetor[j])){
-                palavravetor.push(vetor[1] + " ")
+                palavravetor.push(addTexto)
             }
         }
         palavravetor.push(palavra[i])
     }
-    saida.innerText = palavravetor.join(" ")
-})
+    
+    saida.innerText = palavravetor.join("")
+}
